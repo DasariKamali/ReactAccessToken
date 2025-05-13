159d6245c6366f56da1e713b8ea9f2dead220174
DasariKamali <107987424+DasariKamali@users.noreply.github.com>
2025-05-13 17:09:45 +0530

    Update msalConfig.ts

--- src/auth/msalConfig.ts
+++ src/auth/msalConfig.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
-      clientId: "7a770a40-315a-4348-b170-4323f0b58e0e",
-      authority: "https://login.microsoftonline.com/df1e53b3-1663-4279-8a43-f33a0b883196",
+      clientId: "<ClientID>",
+      authority: "https://login.microsoftonline.com/<TenantID>",
       redirectUri: "http://localhost:3000",
     },
     cache: {
